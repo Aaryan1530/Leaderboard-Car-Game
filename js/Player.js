effdec719e491a1e990ec6bfe86370abd2f85b7c
Aaryan Kumar <aaryansr@gmail.com>
2020-09-19 10:39:58 +0530

    Created Leaderboard

--- js/Player.js
+++ js/Player.js
@@ -3,6 +3,7 @@ class Player {
     this.index = null;
     this.distance = 0;
     this.name = null;
+    this.rank = null;
   }
 
   getCount(){
@@ -31,5 +32,18 @@ class Player {
     playerInfoRef.on("value",(data)=>{
       allPlayers = data.val();
     })
+  }
+  getcarsAtEnd(){
+  var carRef = database.ref('carsAtEnd');
+  carRef.on("value",(data)=>{
+  this.rank = data.val();
+  })
+  
+  }
+  static updatecarsAtEnd(rank){
+  database.ref('/').update({
+    carsAtEnd : rank 
+  })
+
   }
 }
